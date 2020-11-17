e64f511d6a3e6d99152a3807a0830c848537a1a5
Lucas Marques <lukas.marques1995@hotmail.com>
2020-11-17 04:17:54 -0300

    seguranca

--- src/components/comicThumbnail.js
+++ src/components/comicThumbnail.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ComicThumbnail = ({ url, title, thumbnail }) => (
     <React.Fragment>
-        <a className="thumbnail wrapper" href={url} target="_blank">
+        <a className="thumbnail wrapper" href={url} target="_blank" rel='noreferrer'>
             <img className="thumbnail-img" src={thumbnail} alt={thumbnail} />
             <div className="thumbnail-overlay"><span className="thumbnail-text">{title}</span>
             </div>
